ac22bd9f7ec882618c84be6692b27f8ff8d3b339
Birenderjit <birenderjit@gmail.com>
2020-05-30 17:16:53 -0500

    Initial Project Add

---
+++ src/test/java/com/birenderjit/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birenderjit.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
