bde8fb890a495cdd52e905789536e9c8bf6a19d0
Birenderjit <birenderjit@gmail.com>
2020-06-06 22:19:03 -0500

    Refaactoring to common crud interface. Closes #11.

--- pet-clinic-data/src/main/java/com/birenderjit/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/birenderjit/sfgpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.birenderjit.sfgpetclinic.services;
 
 import com.birenderjit.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
