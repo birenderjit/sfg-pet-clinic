937b3e2865a9965e002635d32d0dd823e2a544ef
Birenderjit <birenderjit@gmail.com>
2020-06-08 23:12:47 -0500

    Added specialities Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/com/birenderjit/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.birenderjit.sfgpetclinic.services;
+
+import com.birenderjit.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long>{
+}
