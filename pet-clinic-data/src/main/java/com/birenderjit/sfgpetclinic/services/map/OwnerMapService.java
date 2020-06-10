3dbe5bab97e5c685571c3750d472b27d18356fe4
Birenderjit <birenderjit@gmail.com>
2020-06-10 10:50:10 -0500

    updated profiles for JPA service, tested active profiles. Closes #48

--- pet-clinic-data/src/main/java/com/birenderjit/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/birenderjit/sfgpetclinic/services/map/OwnerMapService.java
@@ -5,11 +5,13 @@ import com.birenderjit.sfgpetclinic.model.Pet;
 import com.birenderjit.sfgpetclinic.services.OwnerService;
 import com.birenderjit.sfgpetclinic.services.PetService;
 import com.birenderjit.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService{
 
     private final PetTypeService petTypeService;
