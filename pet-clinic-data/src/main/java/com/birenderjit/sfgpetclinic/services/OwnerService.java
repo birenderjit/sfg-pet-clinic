d55b46e8584046b314338d469fb6a4129db753ec
Birenderjit <birenderjit@gmail.com>
2020-07-07 13:51:48 -0500

    Implement Find owner. Closes #54

--- pet-clinic-data/src/main/java/com/birenderjit/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/birenderjit/sfgpetclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package com.birenderjit.sfgpetclinic.services;
 
 import com.birenderjit.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
